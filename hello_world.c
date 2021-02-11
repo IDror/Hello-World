aa38fbf89c6ef2116f5c8a634c5974796fbb5c2e
itai <itaidror1@gmail.com>
2021-02-11 16:31:25 +0200

    Adding hello_world.c and hello_world.h

---
+++ hello_world.c
@@ -0,0 +1,11 @@
+//
+// Created by itai on 11/02/2021.
+//
+
+#include "hello_world.h"
+#include <stdio.h>
+
+void main()
+{
+    printf("Hello World\n") ;
+}
---
+++ hello_world.h
@@ -0,0 +1,8 @@
+//
+// Created by itai on 11/02/2021.
+//
+
+#ifndef HELLO_WORLD_HELLO_WORLD_H
+#define HELLO_WORLD_HELLO_WORLD_H
+
+#endif //HELLO_WORLD_HELLO_WORLD_H
